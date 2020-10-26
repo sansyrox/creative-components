ef904599b15be376582522d5227dda83d6c3ccd4
stealthanthrax <sansyrox@gmail.com>
2020-10-27 00:00:56 +0530

    SCSS fix
    Fix SCSS bugs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-import React from 'react'
-import styles from './styles.module.css'
-
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+// import './styles.module.scss'
 
 export { default as Following_Cursor } from '../components/following-cursor'
 export { default as SimpleGrid } from '../layouts/SimpleGrid'
